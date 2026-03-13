1e59cdde392d7389a95d2e1f84f322b267cf7454
Vitor Reis <vitoricmpc@gmail.com>
2026-03-13 16:04:00 -0300

    Teste para salvar arquivos em java

---
+++ semana-2.1/Exercicio08/src/main/java/com/mycompany/exercicio08/Exercicio08.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio08;
+
+/**
+ *
+ * @author vitor
+ */
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
